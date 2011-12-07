c0af7044e1b10e7ff5c879c0ca0a0b022eb8ea0c
unknown <Tjarb@(none)>
2011-12-08 00:00:16 +0100

    Lagt til dynamisk update av chatmessages

---
+++ JoMAR/Controllers/JasonController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JoMAR.Controllers
+{
+    public class JasonController : Controller
+    {
+        //
+        // GET: /Jason/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult getMessages(Guid id)
+        {
+            List<dynamic> list = new List<dynamic>();
+            JodADataContext db = new JodADataContext();
+
+            dynamic msg = (from p in db.ChatMessages
+                           where p.RoomID == id
+                           select p.Text).ToList();
+
+            //foreach (string o in msg)
+            //{
+            //    var myOp = new
+            //    {
+            //        text = o.Text
+
+            //    };
+            //    list.Add(myOp);
+            //}
+            return Json(msg, JsonRequestBehavior.AllowGet);
+
+        }
+
+    }
+}
