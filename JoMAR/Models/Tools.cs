222ac9f6bf23abf5627a3c2c47002ea371be840b
Runar Ingebrigtsen <runar@rin.no>
2011-12-09 05:02:09 +0100

    get random code from random.org

--- JoMAR/Models/Tools.cs
+++ JoMAR/Models/Tools.cs
@@ -5,11 +5,54 @@ using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using System.Security.Cryptography;
+using System.Net;
+using System.IO;
 
 namespace JoMAR.Models
 {
     public class Tools
     {
+        public static string GetCode() { 
+            string randomUrl = "http://www.random.org/strings/?num=1&len=5&digits=on&upperalpha=on&loweralpha=on&unique=on&format=plain&rnd=new";
+            // used to build entire input
+            StringBuilder sb = new StringBuilder();
+
+            // used on each read operation
+            byte[] buf = new byte[8192];
+
+            // prepare the web page we will be asking for
+            HttpWebRequest request = (HttpWebRequest) WebRequest.Create(randomUrl);
+
+            // execute the request
+            HttpWebResponse response = (HttpWebResponse) request.GetResponse();
+
+            // we will read data via the response stream
+            Stream resStream = response.GetResponseStream();
+
+            string tempString = null;
+            int count = 0;
+
+            do
+            {
+                // fill the buffer with data
+                count = resStream.Read(buf, 0, buf.Length);
+
+                // make sure we read some data
+                if (count != 0)
+                {
+                    // translate from bytes to ASCII text
+                    tempString = Encoding.ASCII.GetString(buf, 0, count);
+
+                    // continue building the string
+                    sb.Append(tempString);
+                }
+            }
+            while (count > 0); // any more data to read?
+
+            // return
+            return  sb.ToString();
+        }
+
         public static string Gravatar(string Email, string Alt)
         {
             string imageUrl = "http://www.gravatar.com/avatar/";
